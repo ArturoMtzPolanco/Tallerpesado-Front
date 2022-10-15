cc051b6886feb3411a74c24d4db478c9f8c3f0a6
gerryr8 <92277406+gerryr8@users.noreply.github.com>
2022-10-14 20:11:48 -0500

    estructura gerardo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/admin/home/home.component';
+import {RoutesModule} from "./routes/routes.module";
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/login/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
